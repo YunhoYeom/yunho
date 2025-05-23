1dd17184376667a786a23d6e319041cfb75ca5fe
YunhoYeom <whatever.u.r1103@gmail.com>
2025-05-23 20:32:47 +0900

    first test

---
+++ src/main/java/com/oopsw/yunho/YunhoApplication.java
@@ -0,0 +1,13 @@
+package com.oopsw.yunho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YunhoApplication.class, args);
+	}
+
+}
